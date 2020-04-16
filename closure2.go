18106ff3608f5129dee0a8d5c2b9079753b1cb76
a1101101 <r3901@icloud.com>
2020-04-16 11:35:12 +0900

    Add files via upload

---
+++ closure2.go
@@ -0,0 +1,30 @@
+/*
+関数オブジェクト(クロージャー)を返す関数adder()
+クロージャーは自身のスコープ外(環境)の変数を参照できる
+以下ではadder()内でクロージャー"func(x int) int"が"sum"を参照している
+
+Go言語では、関数オブジェクトを引数にとる、返り値にとる、変数に代入する、のいずれも可能。
+*/
+package main
+
+import "fmt"
+
+//adder() returns a closure
+func adder() func(int) int {
+	sum := 0
+    //closure references sum
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func main() {
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+}
